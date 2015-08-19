96d45e4028b6c45735476762787ec48d398b2b0b
Derek <derek@huement.com>
2015-08-19 16:27:04 -0500

    Spooky Skeletons

--- octoprint_reprasp/__init__.py
+++ octoprint_reprasp/__init__.py
@@ -7,12 +7,12 @@ from __future__ import absolute_import
 
 import octoprint.plugin
 
-class SkeletonPlugin(octoprint.plugin.TemplatePlugin):
+class RepraspPlugin(octoprint.plugin.TemplatePlugin):
 	# TODO Implement me!
 	pass
 
 # If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
 # ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
 # can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
-__plugin_name__ = "Plugin Skeleton"
-__plugin_implementation__ = SkeletonPlugin()
+__plugin_name__ = "RepRasp UI"
+__plugin_implementation__ = RepraspPlugin()
