60dce296bda5c68f4f51d00db58644823b612747
Derek <derek@huement.com>
2015-08-19 21:50:34 -0500

    25

--- octoprint_reprasp/__init__.py
+++ octoprint_reprasp/__init__.py
@@ -23,7 +23,7 @@ class RepraspPlugin(octoprint.plugin.StartupPlugin,
             self._logger.info("RepRasp UI Loaded! (more: %s)" % self._settings.get(["iframeurl"]))
             
     def get_settings_defaults(self):
-            return dict(url="https://www.huement.com")
+            return dict(url="https://www.huement.com",apiurl="reprasp/echo/?name=test")
             #iframeurl=flask.url_for("plugin.reprasp.myEcho"),
             #apiurl="reprasp/echo/?name=test"
             
